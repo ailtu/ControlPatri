02620a046f881cca148355573d25f35231b388df
Ailton cesar <ailtonc.n64@gmail.com>
2022-06-20 22:49:44 -0300

    Initial commit

---
+++ app/src/main/java/com/example/controlpatri/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.controlpatri
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.MotionEvent
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // find your website
+        val findWV = findViewById<View>(R.id.WebView) as WebView
+        findWV.loadUrl("https://www.urbanovitalino.com.br/")
+        findWV.webViewClient = WebViewClient()
+
+        // key steps
+        findWV.canGoBack()
+        findWV.setOnKeyListener(View.OnKeyListener { view, i, keyEvent ->
+            if (i == KeyEvent.KEYCODE_BACK
+                && keyEvent.action == MotionEvent.ACTION_UP
+                && findWV.canGoBack()
+            ) {
+                findWV.goBack()
+                return@OnKeyListener true
+            }
+            false
+        })
+    }
+}
